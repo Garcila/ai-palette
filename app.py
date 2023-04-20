4fbf409672f5fa1bde29822e77e01e1749289f2a
German Arcila <germanarcila@gmail.com>
2023-04-20 08:50:33 -0400

    added os for environment variables in Render

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import openai
 from flask import Flask, render_template, request
 from dotenv import dotenv_values
+from os import getenv
 import json
 
-config = dotenv_values(".env")
+config = dotenv_values(".env") or os.getenv("OPENAI_API_KEY")
 
 openai.api_key = config["OPENAI_API_KEY"]
 
