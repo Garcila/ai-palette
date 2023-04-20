d084e44d1d51115b3d03b323eb9a850cff10ece8
German Arcila <germanarcila@gmail.com>
2023-04-20 10:34:06 -0400

    dotenv issues

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import dotenv_values
 import json
 
 config = dotenv_values(".env")
-
-openai.api_key = config["OPENAI_API_KEY"] or os.getenv("OPENAI_API_KEY")
+# or os.getenv("OPENAI_API_KEY")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
             template_folder='templates',
